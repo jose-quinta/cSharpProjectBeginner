3204659f42a8be66f2b371fe67fdf8b1a013e902
jose quinta <jq1660824@gmail.com>
2026-05-24 12:22:20 -0600

    feat: add guess-number game project

---
+++ guess-number/Program.cs
@@ -0,0 +1,36 @@
+﻿Random aleatorio = new();
+int numeroSecreto = aleatorio.Next(1, 101);
+int intentos = 0;
+bool acertado = false;
+
+Console.WriteLine("=== ADIVINA EL NÚMERO ===");
+Console.WriteLine("He elegido un número entre 1 y 100.");
+Console.WriteLine("¿Puedes adivinar cuál es?\n");
+
+while (!acertado)
+{
+    Console.Write("Ingresa tu número: ");
+    string entrada = Console.ReadLine();
+
+    if (!int.TryParse(entrada, out int guess))
+    {
+        Console.WriteLine("Por favor, ingresa un número válido.");
+        continue;
+    }
+
+    intentos++;
+
+    if (guess < numeroSecreto)
+    {
+        Console.WriteLine("El número es MAYOR.");
+    }
+    else if (guess > numeroSecreto)
+    {
+        Console.WriteLine("El número es MENOR.");
+    }
+    else
+    {
+        acertado = true;
+        Console.WriteLine($"\n Felicidades! Adivinaste el {numeroSecreto} en {intentos} intentos.");
+    }
+}
